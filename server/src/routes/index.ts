6e28f815aef204443903ec8f3419e665f69b8c47
unknown <rosa.angel.daniel@gmail.com>
2024-09-22 20:31:30 -0600

    corrented bug issues in server routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import authRoutes from './auth-routes.js';
-import apiRoutes from './api/index.js';
+import { router as authRoutes} from './auth-routes.js';
+// TODO: uncomment and add apiRoutes ('/api', authenticateToken, apiRoutes)
+// import apiRoutes from './api/index.js'; 
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/api', authenticateToken, apiRoutes);
+router.use('/api', authenticateToken);
 
 export default router;
 
