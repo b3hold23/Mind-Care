562c6eaa0ba489b63a313e9f5a13a650701b973c
Redwolf917 <xander0429@gmail.com>
2024-09-23 19:27:06 -0600

    fixed some navigation stuff

--- client/src/pages/create_profile.tsx
+++ client/src/pages/create_profile.tsx
@@ -39,6 +39,13 @@ const CreateProfile: React.FC = () => {
           alt="Mind Care Logo" 
           className="logo" />
         <h1>Create a New Account</h1>
+        <nav>
+          <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+          </ul>
+        </nav>
       </header>
 
       <main>
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -71,7 +71,7 @@ const HomePage: React.FC = () => {
       <h1 onClick={() => navigate('/home')}>Mind Care</h1>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="/home">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/contact">Contact Us</a></li>
           </ul>
