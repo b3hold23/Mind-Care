e9848ff6abe8277a0b12ee99e73225c96ac5ba20
unknown <rosa.angel.daniel@gmail.com>
2024-09-23 22:00:43 -0600

    added style to contact page

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -9,7 +9,7 @@ const ContactPage: React.FC = () => {
 
   return (
     <div className="contact-container">
-      <header>
+      <header className='welcome-header'>
       <img 
           src={Logo} 
           alt="Mind Care Logo" 
@@ -18,16 +18,16 @@ const ContactPage: React.FC = () => {
         <h1>Contact Us</h1>
         <nav>
           <ul>
-            <li><a href="/home">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact Us</a></li>
+            <li><a className="navBar" href="/home">Home</a></li>
+            <li><a className="navBar" href="/about">About</a></li>
+            <li><a className="navBar" href="/contact">Contact Us</a></li>
           </ul>
         </nav>
       </header>
 
       <main>
         <section className="contact-section">
-          <h2>Get in Touch</h2>
+          <h2>Get in Touch!</h2>
           <p>If you have any questions, feedback, or need support, feel free to reach out to us.</p>
           
           <h3>Contact Information</h3>
